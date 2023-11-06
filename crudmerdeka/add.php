8fbb9afab8cdc995eefb0c53c620c261655eb0cb
MrRavent <gkgalang@gmail.com>
2023-11-06 21:41:17 +0700

    Tambahan

--- crudmerdeka/add.php
+++ crudmerdeka/add.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Tambah Makanan</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+
 <body>
     <div class="container">
         <h1>Tambah Makanan</h1>
@@ -54,9 +56,57 @@
 
         <a href="index.php" class="btn btn-secondary">Kembali</a>
     </div>
+    <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="successModalLabel">Sukses!</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Data makanan berhasil ditambahkan.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+// JavaScript untuk menampilkan pesan sukses atau kesalahan dan modal pop-up
+$(document).ready(function() {
+    const addForm = document.getElementById('addForm');
+    const alertContainer = document.getElementById('alertContainer');
+    const successModal = new bootstrap.Modal(document.getElementById('successModal'));
+
+    addForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const formData = new FormData(addForm);
+
+        // Simulasi pengiriman data dengan delay
+        setTimeout(function() {
+            const randomSuccess = Math.random() < 0.5; // Simulasi kesuksesan acak
+            if (randomSuccess) {
+                alertContainer.innerHTML = '<div class="alert alert-success">Data makanan berhasil ditambahkan.</div>';
+                addForm.reset();
+                successModal.show(); // Tampilkan modal pop-up sukses
+            } else {
+                alertContainer.innerHTML = '<div class="alert alert-danger">Terjadi kesalahan saat menambahkan data makanan.</div>';
+            }
+        }, 1000); // Delay 1 detik (digunakan untuk simulasi)
+
+        // Hapus pesan setelah beberapa detik
+        setTimeout(function() {
+            alertContainer.innerHTML = '';
+        }, 5000); // Hapus pesan setelah 5 detik
+    });
+});
+</script>
+
 </body>
+
 </html>
--- crudmerdeka/index.php
+++ crudmerdeka/index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Daftar Makanan</title>
     <!-- Sisipkan file Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 </head>
+
 <body>
     <div class="container">
         <h1>Daftar Makanan</h1>
@@ -28,13 +32,13 @@
         $result = mysqli_query($koneksi, $query);
 
         if ($result) {
-            echo "<table class='table table-striped'>
+            echo "<table id='makananTable' class='table table-striped'>
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Nama Makanan</th>
-                        <th>Deskripsi</th>
-                        <th>Harga</th>
+                        <th data-sortable='true'>ID</th>
+                        <th data-sortable='true'>Nama Makanan</th>
+                        <th data-sortable='true'>Deskripsi</th>
+                        <th data-sortable='true'>Harga</th>
                         <th>Action</th>
                     </tr>
                 </thead>
@@ -67,5 +71,59 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Sisipkan JavaScript tambahan untuk mempercantik tampilan -->
+    <script>
+        $(document).ready(function() {
+            // Menambahkan kelas "table-bordered" untuk memberi tabel border
+            $('#makananTable').addClass('table-bordered');
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('#makananTable').addClass('table-bordered');
+
+            // Fungsi konfirmasi saat tombol "Delete" diklik
+            $('.btn-danger').click(function() {
+                return confirm('Apakah Anda yakin ingin menghapus item ini?');
+            });
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('#makananTable').addClass('table-bordered');
+
+            // Fungsi hover efek untuk baris tabel
+            $('#makananTable tbody tr').hover(
+                function() {
+                    $(this).addClass('table-info');
+                },
+                function() {
+                    $(this).removeClass('table-info');
+                }
+            );
+        });
+    </script>
+    <script>
+$(document).ready(function() {
+    $('#makananTable').addClass('table-bordered');
+
+    // Inisialisasi DataTables untuk fungsi pencarian
+    $('#makananTable').DataTable();
+});
+
+</script>
+<script>
+    $(document).ready(function() {
+        $('#makananTable').addClass('table-bordered');
+
+        // Inisialisasi DataTables untuk fungsi pencarian dan sorting
+        $('#makananTable').DataTable({
+            "order": [] // Parameter ini akan mengaktifkan sorting pada semua kolom
+        });
+    });
+</script>
+
 </body>
+
 </html>
